a03b6ffdd4e4f5bbae0a124a5281712815a14acb
vasy <007vasy@gmail.com>
2018-01-24 11:34:20 +0100

    package FIX5

--- att_gen_for_six_forklift.R
+++ att_gen_for_six_forklift.R
@@ -18,8 +18,10 @@ d_wheel = 0.467 # from catalog
 
 #resolution for factor matrix
 reso_m = 7 #must be odd!!!
+print("asd1!")
 if(reso_m%%2!=1)
   print("Must be odd!")
+print("asd2!")
 speed_max = 5000
 torque_max = 80
 
@@ -91,7 +93,7 @@ for(file_name_i in wd_filenames)
     #make new column in fp_df
     fp_df = mutate(fp_df, temp_col = as.numeric("NA"))
     
-    print("asd!")
+ 
     
     temp_df = temp_list[[w_column]] %>%
       as.data.frame() %>%
