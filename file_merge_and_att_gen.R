ac4df5590d024d95fe2da00f1640e9411c0c01eb
vasy <007vasy@gmail.com>
2018-01-30 12:02:34 +0100

    file concat

--- file_merge_and_att_gen.R
+++ file_merge_and_att_gen.R
@@ -1,1 +1,34 @@
 # file concat and att gen for the six forklift
+library(readr)
+library(purrr)
+library(plyr)
+library(dplyr)
+library(ggplot2)
+library(stringr)
+library(lubridate)
+
+#PAKS3 (batman)
+setwd("/home/vassb/fingerprint_data/ansgar_att_six_forklift/")
+export_location="/home/vassb/fingerprint_data/ansgar_att_six_forklift_merged/"
+
+wd_filenames = as.data.frame(list.files(pattern = '\\.csv$'),col.names = 'filenames') %>%
+  mutate(truck_cat = function(x){
+    if(str_detect(x,'Daimler F 00150'))
+      return(as.factor('Daimler F 00150'))
+    else if(str_detect(x,'Daimler F 00166'))
+      return(as.factor('Daimler F 00166'))
+    else if(str_detect(x,'Daimler F 00192'))
+      return(as.factor('Daimler F 00192'))
+    else if(str_detect(x,'Imperial_D_00125'))
+      return(as.factor('Imperial_D_00125'))
+    else if(str_detect(x,'IMRl_F_00214'))
+      return(as.factor('IMRl_F_00214'))
+    else if(str_detect(x,'Daimler F 00166'))
+      return(as.factor('Schenker_C_01115'))
+    }
+  ) %>%
+  groupby(truck_cat) %>%
+  arrange(filenames)
+print(wd_filenames)
+
+#for(i in i:)
