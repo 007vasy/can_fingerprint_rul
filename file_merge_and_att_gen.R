f8639d7bd9fef08f72d2fbebcbfe86b28c6d7cbd
vasy <007vasy@gmail.com>
2018-01-31 10:21:41 +0100

    file sorting

--- file_merge_and_att_gen.R
+++ file_merge_and_att_gen.R
@@ -8,28 +8,36 @@ library(stringr)
 library(lubridate)
 
 #PAKS3 (batman)
-setwd("/home/vassb/fingerprint_data/ansgar_att_six_forklift/")
-export_location="/home/vassb/fingerprint_data/ansgar_att_six_forklift_merged/"
+setwd("/media/vasy/Data/Doksik/projekts/AITIA/reduced_can_fp/")
+export_location="/media/vasy/Data/Doksik/projekts/AITIA/reduced_can_fp/merged_machines/"
 
-wd_filenames = as.data.frame(list.files(pattern = '\\.csv$'),col.names = 'filenames') %>%
-  transform(filenames = as.character(filenames)) %>%
-  mutate(truck_cat = function(x = filenames){
-    if(str_detect(x,'Daimler F 00150'))
-      return('Daimler F 00150')
-    else if(str_detect(x,'Daimler F 00166'))
-      return('Daimler F 00166')
-    else if(str_detect(x,'Daimler F 00192'))
-      return('Daimler F 00192')
-    else if(str_detect(x,'Imperial_D_00125'))
-      return('Imperial_D_00125')
-    else if(str_detect(x,'IMRl_F_00214'))
-      return('IMRl_F_00214')
-    else if(str_detect(x,'Daimler F 00166'))
-      return('Schenker_C_01115')
-    }
-  ) %>%
-  groupby(truck_cat) %>%
-  arrange(filenames)
-print(wd_filenames)
+truck_category <- function(x){
+  if(str_detect(x,'Daimler F 00150'))
+    return('Daimler F 00150')
+  else if(str_detect(x,'Daimler F 00166'))
+    return('Daimler F 00166')
+  else if(str_detect(x,'Daimler F 00192'))
+    return('Daimler F 00192')
+  else if(str_detect(x,'Imperial_D_00125'))
+    return('Imperial_D_00125')
+  else if(str_detect(x,'IMRl_F_00214'))
+    return('IMRl_F_00214')
+  else if(str_detect(x,'Schenker_C_01115'))
+    return('Schenker_C_01115')
+  else
+    return(NA)
+}
+
+wd_filenames = as.data.frame(list.files(pattern = '\\.csv$'))
+names(wd_filenames) =  'filenames'
+
+for(i in 1:length(wd_filenames$filenames)){
+  wd_filenames$truck_cat[i]=truck_category(wd_filenames$filenames[i])
+}
+ wd_filenames$truck_cat = as.factor(wd_filenames$truck_cat)
+
+cat_files = group_by(wd_filenames,truck_cat) %>%
+  arrange(.by_group = TRUE)
+print(cat_files)
 
 #for(i in i:)
